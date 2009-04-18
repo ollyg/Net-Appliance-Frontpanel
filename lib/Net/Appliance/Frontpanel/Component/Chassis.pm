edde983e4b3c50e8eeae49ca629ed1667464a36a
Oliver Gorwits <oliver@cpan.org>
2009-04-18 09:40:52 +0000

    base the chassis off component

--- lib/Net/Appliance/Frontpanel/Component/Chassis.pm
+++ lib/Net/Appliance/Frontpanel/Component/Chassis.pm
@@ -1,23 +1,12 @@
 package Net::Appliance::Frontpanel::Chassis;
 use Moose;
 
-with qw(
-    Net::Appliance::Frontpanel::Personality
-    Net::Appliance::Frontpanel::Component::ImageMap
-    Net::Appliance::Frontpanel::Component::Transforms
-);
+extends 'Net::Appliance::Frontpanel::Component';
 
 has ip => (
     is => 'ro',
     isa => 'Str',
     required => 1,
-);
-
-has 'config' => (
-    is => 'ro',
-    isa => 'Net::Appliance::Frontpanel::Config',
-    required => 1,
-    weak_ref => 1,
 );
 
 has spec => (
@@ -54,13 +43,6 @@ sub transpose_and_paste {
 
 sub BUILD {
     my ($self, $params) = @_;
-
-    # load up the image type personality
-    $params->{img_type} ||= 'Imager';
-    $self->apply_personality(
-        'Net::Appliance::Frontpanel::Image',
-        $params->{img_type}
-    );
 
     foreach my $device ($self->spec) {
         my $module = $self->make_module($device);
