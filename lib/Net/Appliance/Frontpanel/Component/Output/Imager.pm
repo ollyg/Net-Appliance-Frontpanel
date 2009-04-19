fb65a3d29b8171513a91b5be6afcf0b7826ca990
Oliver Gorwits <oliver@cpan.org>
2009-04-19 17:07:24 +0000

    shuffle files around

--- lib/Net/Appliance/Frontpanel/Component/Output/Imager.pm
+++ lib/Net/Appliance/Frontpanel/Component/Output/Imager.pm
@@ -35,3 +35,21 @@ sub paste_into {
 no Moose::Role;
 1;
 __END__
+package Net::Appliance::Frontpanel::Component::Image::Base;
+use Moose::Role;
+
+has image => (
+    is => 'rw',
+    isa => 'Object',
+    lazy_build => 1,
+);
+
+sub paste_into_self {
+    my $self = shift;
+    return $self->paste_into(@_, parent => $self->image);
+}
+
+no Moose::Role;
+1;
+__END__
+
