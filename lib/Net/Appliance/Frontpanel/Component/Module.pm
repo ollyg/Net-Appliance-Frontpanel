ae57048026bebca71a3c6f9255b34e59b03b4df8
Oliver Gorwits <oliver@cpan.org>
2009-04-22 19:46:54 +0000

    remove redundant code to process children

--- lib/Net/Appliance/Frontpanel/Component/Module.pm
+++ lib/Net/Appliance/Frontpanel/Component/Module.pm
@@ -10,58 +10,46 @@ has spec => (
     required => 1,
 );
 
+sub process_children {
+    my ($self, $type) = @_;
+
+    my $child_class_for = { modules => 'Module', ports => 'Port' };
+    my $child_class = 'Net::Appliance::Frontpanel::Component::'. $child_class_for->{$type};
+
+    foreach my $item (@{$self->spec->{$type}}) {
+        my $child = $child_class->new({
+            config => $self->config,
+            spec => { %$item, ports_data => $self->spec->{ports_data}, ip => $self->spec->{ip} },
+        });
+
+        # rotate imagemap and image
+        if (my $rotate = $item->{rotate} % 360) {
+            $child->rotate_map_by($rotate);
+            $child->image( $child->image->rotate(right => $rotate) );
+        }
+
+        # transpose and append imagemap
+        $child->transpose_map(x => $item->{x}, y => $item->{y});
+        $self->imagemap( $self->imagemap . $child->imagemap );
+
+        # transpose and copy image
+        $self->paste_into_self(
+            child => $child->image,
+            x     => $item->{x},
+            y     => $item->{y},
+        );
+    }
+
+    return $self;
+}
+
+
 sub BUILD {
     my ($self, $params) = @_;
 
     $self->image->read(file => $self->config->image_loc($self->spec->{image}));
-
-    foreach my $item (@{$self->spec->{modules}}) {
-        my $module = Net::Appliance::Frontpanel::Component::Module->new({
-            config => $self->config,
-            spec => { %$item, ports_data => $self->spec->{ports_data}, ip => $self->spec->{ip} },
-        });
-
-        # rotate imagemap and image
-        if (my $rotate = $item->{rotate} % 360) {
-            $module->rotate_map_by($rotate);
-            $module->image( $module->image->rotate(right => $rotate) );
-        }
-
-        # transpose and append imagemap
-        $module->transpose_map(x => $item->{x}, y => $item->{y});
-        $self->imagemap( $self->imagemap . $module->imagemap );
-
-        # transpose and copy image
-        $self->paste_into_self(
-            child => $module->image,
-            x     => $item->{x},
-            y     => $item->{y},
-        );
-    }
-
-    foreach my $item (@{$self->spec->{ports}}) {
-        my $port = Net::Appliance::Frontpanel::Component::Port->new({
-            config => $self->config,
-            spec => { %$item, ports_data => $self->spec->{ports_data}, ip => $self->spec->{ip} },
-        });
-
-        # rotate imagemap and image
-        if (my $rotate = $item->{rotate} % 360) {
-            $port->rotate_map_by($rotate);
-            $port->image( $port->image->rotate(right => $rotate) );
-        }
-
-        # transpose and append imagemap
-        $port->transpose_map(x => $item->{x}, y => $item->{y});
-        $self->imagemap( $self->imagemap . $port->imagemap );
-
-        # transpose and copy image
-        $self->paste_into_self(
-            child => $port->image,
-            x     => $item->{x},
-            y     => $item->{y},
-        );
-    }
+    $self->process_children('modules');
+    $self->process_children('ports');
 }
 
 no Moose;
